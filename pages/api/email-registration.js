8c6c5fb632a4a127e2a9a3ea29475e9d3879de0c
Bnaya Kahalani <bnaya50@gmail.com>
2023-03-19 10:37:12 +0200

    add buildPath and extractData functions to email-registration API

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -1,6 +1,27 @@
+import path from 'path'
+import fs from 'fs'
+
+function bulidPath() {
+  return path.join(process.cwd, 'data', 'data.json')
+}
+
+function extractData(filePath) {
+  const jsonData = fs.readFileSync(filePath)
+  const data = JSON.parse(jsonData)
+  return data
+}
+
 export default function handler(req, res) {
   const { method } = req
+
+  const filePath = bulidPath()
+  const { meetups_categories, all_meetups } = extractData(filePath)
+
+
   if (method === "POST") {
+    const { email, meetupId } = req.body
+
+    res.status(200).json({ message: `You have benn successfully registered with the email: ${email}` })
 
   }
 }
